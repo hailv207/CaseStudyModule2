2d648e5a390030c7d14c2ad01aae460895a0414b
vuhaile <levuhai207@hotmail.com.vn>
2020-07-01 08:55:54 +0700

    update

--- src/application/employee/EmployeeManagerController.java
+++ src/application/employee/EmployeeManagerController.java
@@ -9,11 +9,13 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 public class EmployeeManagerController implements Initializable {
-    @FXML
     private ObservableList<Employee> employeeList;
 
     @FXML
@@ -41,20 +43,9 @@ public class EmployeeManagerController implements Initializable {
     private TableColumn<Employee, Boolean> statusColumn;
 
 
-    public void addEmployee() {
-    }
-
-    public void removeEmployee() {
-
-    }
-
-    public void editEmployee() {
-
-    }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        File file = new File("application/employee/data/employees.dat");
+        File file = new File("/Users/vuhaile/CodeGym/CaseStudyModule2/src/application/employee/data/employees.dat");
         if (!file.exists()) {
             try {
                 file.createNewFile();
@@ -63,13 +54,33 @@ public class EmployeeManagerController implements Initializable {
             }
         }
         try {
+            FileOutputStream fos = new FileOutputStream(file,true);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(new Employee("hailv", "Le Vu Hai", "Ha Noi", "197196259", "Manager", true));
+            oos.writeObject(new Employee("hylv", "Nguyen Van A", "Ha Noi", "197188659", "Staff", true));
+            fos.close();
+            oos.close();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        try {
             FileInputStream fis = new FileInputStream(file);
             ObjectInputStream ois = new ObjectInputStream(fis);
-            employeeList = FXCollections.observableArrayList((Employee)ois.readObject());
+
+            employeeList = FXCollections.<Employee>observableArrayList();
+            Object obj = null;
+           while((obj = ois.readObject()) != null){
+               employeeList.add((Employee)obj);
+           }
+            fis.close();
+            ois.close();
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-
+//        employeeList = FXCollections.observableArrayList(
+//                new Employee("hailv","Le Vu Hai","Ha Noi","197196259","Manager",true),
+//                new Employee("hylv","Nguyen Van A","Ha Noi","197188659","Staff",true)
+//        );
         codeColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("employeeCode"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("employeeName"));
         idnumberColumn.setCellValueFactory(new PropertyValueFactory<Employee, String>("employeeIDNumber"));
@@ -78,4 +89,5 @@ public class EmployeeManagerController implements Initializable {
         statusColumn.setCellValueFactory(new PropertyValueFactory<Employee, Boolean>("status"));
         employeeTableView.setItems(employeeList);
     }
+
 }
--- src/application/employee/EmployeeTest.java
+++ src/application/employee/EmployeeTest.java
@@ -1,10 +1,13 @@
 package application.employee;
 
+import application.login.Main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.net.URL;
 
 public class EmployeeTest extends Application {
     public static void main(String[] args) {
